2f7347b483c1023956701ff59e8d3bda1d871e52
Anekoique <ctolu01@gmail.com>
2025-03-31 23:30:23 +0800

    feat: add comment

--- axorigin/src/main.rs
+++ axorigin/src/main.rs
@@ -1,19 +1,11 @@
 #![no_std]
 #![no_main]
+mod lang_items;                                         // 需要实现panic handler处理不可恢复异常
 
-mod lang_items;
-
-#[unsafe(no_mangle)]
-#[unsafe(link_section = ".text.boot")]
+#[unsafe(no_mangle)]                                    // 要求编译器保持_start函数名称
+#[unsafe(link_section = ".text.boot")]                  // 将_start放置在链接文件标记处
 unsafe extern "C" fn _start() -> ! {
-    core::arch::asm!(
-        "wfi",
-        options(noreturn),
-    );
+    unsafe {
+        core::arch::asm!("wfi", options(noreturn),);    // 嵌入一条汇编
+    }
 }
-
-
-
-
-
-
---
+++ axorigin/target/debug/deps/axorigin-4339bd3058a422aa.d
@@ -0,0 +1,10 @@
+/home/ctoluu/OSCamp-25s/ArceOS/axorigin/target/debug/deps/libaxorigin-4339bd3058a422aa.rmeta: src/main.rs src/lang_items.rs Cargo.toml
+
+/home/ctoluu/OSCamp-25s/ArceOS/axorigin/target/debug/deps/axorigin-4339bd3058a422aa.d: src/main.rs src/lang_items.rs Cargo.toml
+
+src/main.rs:
+src/lang_items.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
